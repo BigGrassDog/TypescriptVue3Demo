63dce5bab7224d543f4f1a7c4e3e5c882b268aff
huzun <654141802@qq.com>
2022-05-19 17:33:12 +0800

    I love summer

---
+++ 01_Typescript/03_webpack_ts/src/01_基础类型.ts
@@ -0,0 +1,33 @@
+// 基础类型
+(() => {
+  // 布尔类型----> boolean
+  // 基本语法
+  // let 变量名:数据类型 = 值
+  let flag: boolean = true
+  console.log(flag);
+
+  // 数字类型----> number
+  let a1: number = 10 // 十进制
+  let a2: number = 0b1010 // 二进制
+  let a3: number = 0o12 // 八进制
+  let a4: number = 0xa // 十六进制
+  console.log(a1);
+  console.log(a2);
+  console.log(a3);
+  console.log(a4);
+
+  //字符串类型----> string
+  let str1: string = '宇多田光'
+  let str2: string = '广末凉子'
+  let str3: string = '中森明菜'
+  let str4: string = '坂井泉水'
+  console.log(`${str1},${str2},${str3},${str4}`);
+
+  // 字符串和数字之间能够一起拼接
+  let str5:string = '广末凉子天下第一'
+  let num:number = 1000
+  console.log(str5 + num);
+  
+  //总结：ts中变量一开始是什么类型，那么后期赋值的时候，只能用这个类型的数据，是不允许用其他类型的数据赋值给当前的这个变量
+
+})()
---
+++ 01_Typescript/03_webpack_ts/src/main.ts
@@ -0,0 +1,2 @@
+import './01_基础类型.ts'
+document.write('广末凉子')
