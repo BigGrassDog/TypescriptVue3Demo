3d230b5c441abf5bc66190b427c6524452329683
huzun <654141802@qq.com>
2022-05-25 09:53:19 +0800

    gd update

--- 01_Typescript/03_webpack_ts/src/01_基础类型.ts
+++ 01_Typescript/03_webpack_ts/src/01_基础类型.ts
@@ -24,22 +24,55 @@
   console.log(`${str1},${str2},${str3},${str4}`);
 
   // 字符串和数字之间能够一起拼接
-  let str5:string = '广末凉子天下第一'
-  let num:number = 1000
+  let str5: string = '广末凉子天下第一'
+  let num: number = 1000
   console.log(str5 + num);
-  
+
   //总结：ts中变量一开始是什么类型，那么后期赋值的时候，只能用这个类型的数据，是不允许用其他类型的数据赋值给当前的这个变量
 
   console.log('---------------------------');
-  
-  let und:undefined = undefined
-  let nll:null = null
+
+  let und: undefined = undefined
+  let nll: null = null
   console.log(und);
   console.log(nll);
   // undefined 和 null 都可以作为其他类型的子类型，把 undefined 和 null 赋值给其他类型的变量的，如:number 类型的变量
   // let num2:number = undefined
-  let num2:number = null
-  console.log(num2);
-  
+  let num2: number = null
+  // console.log(num2);
+
+  console.log('----------------------------');
+
+  // 数组类型
+  // 数组定义方式1
+  let arr1: number[] = [1, 2, 3, 4, 5, 6]
+  // 数组定义方式2：泛型写法
+  let arr2: Array<number> = [1, 2, 3, 4, 5, 6]
+
+  // 注意问题：数组定义后，里面的数据类型必须和定义数组的时候的类型是一致的，否则有错误提示信息，也不会编译通过
+
+  // 元组类型：在定义数组的时候，类型和数据的个数一开始就已经限定了
+  let arr3: [string, number, boolean] = ['广末凉子', 100, true]
+  // 注意问题：元组类型在使用的时候，数据的类型的位置和数据的个数，应该在和定义元组的时候的数据类型及位置应该是一致的
+
+  console.log('---------------------------');
+
+  // 枚举类型
+  enum Color {
+    red = 1,
+    green,
+    blue
+  }
+  // 定义一个Color的枚举类型的变量来接收枚举的值
+  let color: Color = Color.red
+  console.log(color);
+  console.log(Color.red, Color.green, Color.blue);
+  console.log(Color[3]);
+  // 小例子，枚举中的元素可以是中文的数据值，但是不推荐
+  enum Gender {
+    女,
+    男
+  }
+  console.log(Gender.男);
 
 })()
