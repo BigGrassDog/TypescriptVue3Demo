28009e1a90725967df30f65717e1d3b996a80a84
huzun <654141802@qq.com>
2022-06-29 19:58:48 +0800

    I love summer

---
+++ 01_Typescript/03_webpack_ts/src/01_类.ts
@@ -0,0 +1,29 @@
+// 类：可与理解为模板，通过模板可以实例化兑现
+// 面向对象的编程思想
+(()=>{
+// ts中类的定义及适使用
+class Person{
+  // 定义属性
+  name:string
+  age:number
+  gender:string
+
+  // 定义构造函数：为了将来实例化对象的时候，可以直接对属性的值进行初始化
+  constructor(name:string,age:number,gender:string) {
+    // 更新对象中的属性数据
+    this.name = name
+    this.age = age
+    this.gender = gender
+  }
+
+  // 定义实例方法
+  sayHi(str:string){
+    console.log(`大家好，我是${this.name},今年${this.age},岁了,是个${this.gender}孩子,` + str);
+    
+  }
+}
+
+const person1 = new Person('广末凉子',18,'女')
+
+person1.sayHi('啊哈哈')
+})()
--- 01_Typescript/03_webpack_ts/src/main.ts
+++ 01_Typescript/03_webpack_ts/src/main.ts
@@ -1,5 +1,6 @@
 // import './01_基础类型.ts'
 // import './01_接口.ts'
 // import './02_函数类型.ts'
-import './03_类类型.ts'
+// import './03_类类型.ts'
+import './01_类.ts'
 document.write('广末凉子天下第一')
