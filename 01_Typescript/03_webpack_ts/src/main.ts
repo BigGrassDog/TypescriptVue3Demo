3f45bfd761d90e6aea5f78fcd06c262daeca2d00
huzun <654141802@qq.com>
2022-06-30 17:26:54 +0800

    I love summer

--- 01_Typescript/03_webpack_ts/src/main.ts
+++ 01_Typescript/03_webpack_ts/src/main.ts
@@ -2,5 +2,7 @@
 // import './01_接口.ts'
 // import './02_函数类型.ts'
 // import './03_类类型.ts'
-import './01_类.ts'
+// import './01_类.ts'
+// import './02_继承.ts'
+import './03_多态.ts'
 document.write('广末凉子天下第一')
