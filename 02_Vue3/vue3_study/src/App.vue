d86cd65a9636c03d5ecaf2f3f0b5c7991d165887
huzun <654141802@qq.com>
2022-07-15 16:50:17 +0800

    update

--- 02_Vue3/vue3_study/src/App.vue
+++ 02_Vue3/vue3_study/src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent,ref } from 'vue';
 
 export default defineComponent({
   name: 'App',
@@ -26,10 +26,16 @@ export default defineComponent({
   // setup 是组合 api 的入口函数
   setup(){
     // 变量
-    let count = 0
+    // let count = 0 // 此时的数据并不是响应式的数据（响应式数据：数据变化，页面跟着渲染变化）
+    // ref 是一个函数，作用：定义一个响应式的数据，返回的是一个 Ref 对象，对象中有一个value属性，如果需要对数据进行操作，需要使用该 Ref 对象调用 value 属性的方式进行数据的操作
+    // html 模板中是不需要使用 .value 属性的写法
+    // 一般用来定义一个基本类型的响应式数据
+    let count = ref(0)
     // 方法
     function updateCount(){
-      count ++
+      console.log('======');
+      
+      count.value ++
     }
 
     // 返回的是一个对象
