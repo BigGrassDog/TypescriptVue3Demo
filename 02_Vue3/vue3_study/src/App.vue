410730e838596941ac7eb96fcf1f4c6de49378e1
huzun <654141802@qq.com>
2022-07-15 13:56:55 +0800

    hot hot hot

--- 02_Vue3/vue3_study/src/App.vue
+++ 02_Vue3/vue3_study/src/App.vue
@@ -1,27 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<h2>setup和ref的基本使用</h2>
+<h3>{{count}}</h3>
+<button @click="updateCount">按钮</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  // 需求：页面打开后可以直接看到一个数据，点击按钮后，该数据可以发生变化
+  // vue2的方式实现
+  // data(){
+  //   return {
+  //     count:0
+  //   }
+  // },
+  // methods:{
+  //   updateCount(){
+  //     this.count ++
+  //   }
+  // }
+
+  // vue3的方式实现
+  // setup 是组合 api 的入口函数
+  setup(){
+    // 变量
+    let count = 0
+    // 方法
+    function updateCount(){
+      count ++
+    }
+
+    // 返回的是一个对象
+    return {
+      count,
+      updateCount
+    }
   }
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
