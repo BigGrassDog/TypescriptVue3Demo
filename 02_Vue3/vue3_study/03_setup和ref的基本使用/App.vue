898a6ce05376a248446c1a3185f4bea4bfed9018
gd <shengxiaoan@icloud.com>
2022-07-17 20:53:40 +0800

    It's raning

--- 02_Vue3/vue3_study/03_setup和ref的基本使用/App.vue
+++ 02_Vue3/vue3_study/03_setup和ref的基本使用/App.vue
@@ -1,11 +1,48 @@
 <template>
-<div></div>
+<h2>setup和ref的基本使用</h2>
+<h3>{{count}}</h3>
+<button @click="updateCount">按钮</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent,ref } from 'vue';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  // 需求：页面打开后可以直接看到一个数据，点击按钮后，该数据可以发生变化
+  // vue2的方式实现
+  // data(){
+  //   return {
+  //     count:0
+  //   }
+  // },
+  // methods:{
+  //   updateCount(){
+  //     this.count ++
+  //   }
+  // }
+
+  // vue3的方式实现
+  // setup 是组合 api 的入口函数
+  setup(){
+    // 变量
+    // let count = 0 // 此时的数据并不是响应式的数据（响应式数据：数据变化，页面跟着渲染变化）
+    // ref 是一个函数，作用：定义一个响应式的数据，返回的是一个 Ref 对象，对象中有一个value属性，如果需要对数据进行操作，需要使用该 Ref 对象调用 value 属性的方式进行数据的操作
+    // html 模板中是不需要使用 .value 属性的写法
+    // 一般用来定义一个基本类型的响应式数据
+    let count = ref(0)
+    // 方法
+    function updateCount(){
+      console.log('======');
+      
+      count.value ++
+    }
+
+    // 返回的是一个对象
+    return {
+      count,
+      updateCount
+    }
+  }
 });
 </script>
--- 02_Vue3/vue3_study/src/App.vue
+++ 02_Vue3/vue3_study/src/App.vue
@@ -1,47 +1,62 @@
 <template>
-<h2>setup和ref的基本使用</h2>
-<h3>{{count}}</h3>
-<button @click="updateCount">按钮</button>
+<h2>reactive 的使用</h2>
+<h3>名字：{{user.name}}</h3>
+<h3>年龄：{{user.age}}</h3>
+<h3>媳妇儿：{{user.wife}}</h3>
+<button @click="updateUser">修改数据</button>
 </template>
 
 <script lang="ts">
-import { defineComponent,ref } from 'vue';
+import { defineComponent,reactive } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  // 需求：页面打开后可以直接看到一个数据，点击按钮后，该数据可以发生变化
-  // vue2的方式实现
-  // data(){
-  //   return {
-  //     count:0
-  //   }
-  // },
-  // methods:{
-  //   updateCount(){
-  //     this.count ++
-  //   }
-  // }
 
-  // vue3的方式实现
-  // setup 是组合 api 的入口函数
+  // 需求：显示用户的相关信息，点击按钮，可以更新用户的相关信息数据
+
+  /**
+   * 作用: 定义多个数据的响应式
+   * const proxy = reactive(obj): 接收一个普通对象然后返回该普通对象的响应式代理器对象
+   * 响应式转换是“深层的”：会影响对象内部所有嵌套的属性
+   * 内部基于 ES6 的 Proxy 实现，通过代理对象操作源对象内部数据都是响应式的
+   */
+
   setup(){
-    // 变量
-    // let count = 0 // 此时的数据并不是响应式的数据（响应式数据：数据变化，页面跟着渲染变化）
-    // ref 是一个函数，作用：定义一个响应式的数据，返回的是一个 Ref 对象，对象中有一个value属性，如果需要对数据进行操作，需要使用该 Ref 对象调用 value 属性的方式进行数据的操作
-    // html 模板中是不需要使用 .value 属性的写法
-    // 一般用来定义一个基本类型的响应式数据
-    let count = ref(0)
-    // 方法
-    function updateCount(){
-      console.log('======');
-      
-      count.value ++
+    const obj:any = {
+      name:'xiaoming',
+      age:20,
+      wife:{
+        name:'xiaohong',
+        age:20,
+        cars:['奔驰','宝马','奥迪']
+      }
     }
 
-    // 返回的是一个对象
+    // 把数据变成响应式数据
+    // 返回的是一个 proxy 的代理对象，被代理的目标对象就是 obj 对象
+    // user 现在就是代理对象，obj 是目标对象
+    // user 对象的类型是 Proxy
+    const user = reactive(obj)
+    console.log(user);    
+
+    // 方法
+    const updateUser = ()=>{
+      // 直接使用目标对象的方式来更新目标对象中的成员的值，是不可能的，只能使用代理对象的方式来更新数据（响应式数据）
+      // obj.name += '======'
+      // 下面的可以
+      // user.name += '=='
+      // user.age += 2
+      // user.wife.name += '++'
+      // user.wife.cars[0] = '玛莎拉蒂'
+
+      // user ----> 代理对象；user ------> 目标对象
+      // user 对象或者 obj 对象添加一个新的属性，哪一种方式会影响界面的更新
+      obj.gender = '男'
+      // user 对象或者 obj 对象中移除一个已经存在的属性，哪一种方式会影响界面的更新
+    }
     return {
-      count,
-      updateCount
+      user,
+      updateUser
     }
   }
 });
