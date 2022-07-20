4ec51991d9363aabfabec7a313e21f702bf65319
gd <shengxiaoan@icloud.com>
2022-07-20 21:53:15 +0800

    update

--- 02_Vue3/vue3_study/src/App.vue
+++ 02_Vue3/vue3_study/src/App.vue
@@ -1,15 +1,21 @@
 <template>
 <h1>App父级组件</h1>
+<hr />
+<child-component></child-component>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+// 引入子级组件Child
+import ChildComponent from './components/ChildComponent.vue'
 export default defineComponent({
   name:'App',
+  // 注册组件
+  components:{
+    ChildComponent
+  },
   setup () {
-    
-
     return {}
   }
 })
