16b649948b20211fcf3d3d1338cad52562ddd73e
gd <shengxiaoan@icloud.com>
2022-07-24 21:14:09 +0800

    hooks

---
+++ 02_Vue3/vue3_study/09_生命周期/App.vue
@@ -0,0 +1,27 @@
+<template>
+<h2>App 父级组件</h2>
+<button @click="isShow = !isShow">切换显示</button>
+<hr />
+<child-component v-if="isShow"></child-component>
+</template>
+
+<script lang="ts">
+import ChildComponent from './components/ChildComponent.vue'
+import { defineComponent,ref } from 'vue'
+export default defineComponent({
+  name:'App',
+  // 注册组件
+  components:{
+    ChildComponent
+  },
+  setup() {
+    const isShow = ref(true)
+    return {
+      isShow
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
--- 02_Vue3/vue3_study/src/App.vue
+++ 02_Vue3/vue3_study/src/App.vue
@@ -1,27 +1,24 @@
 <template>
-<h2>App 父级组件</h2>
-<button @click="isShow = !isShow">切换显示</button>
-<hr />
-<child-component v-if="isShow"></child-component>
+  <h2>自定义 hook 函数操作</h2>
+  <h2>x:{{x}},y:{{y}}</h2>
 </template>
 
 <script lang="ts">
-import ChildComponent from './components/ChildComponent.vue'
-import { defineComponent,ref } from 'vue'
+import { defineComponent } from 'vue'
+import useMousePosition from './hooks/useMousePositions'
+
 export default defineComponent({
   name:'App',
-  // 注册组件
-  components:{
-    ChildComponent
-  },
-  setup() {
-    const isShow = ref(true)
+  // 需求1:用户在页面中点击页面，把惦记的位置的纵横坐标收集起来并展示出来
+  setup () {
+    const {x,y} = useMousePosition()
     return {
-      isShow
+      x,
+      y
     }
   }
 })
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 </style>
