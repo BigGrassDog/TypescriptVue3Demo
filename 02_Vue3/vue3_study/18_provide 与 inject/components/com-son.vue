80d62f121b039a0cfc081e52ad57fcf4c5e62c08
gd <shengxiaoan@icloud.com>
2022-08-01 21:54:22 +0800

    provide 与 inject

---
+++ 02_Vue3/vue3_study/18_provide 与 inject/components/com-son.vue
@@ -0,0 +1,23 @@
+<template>
+<h3>Son子级组件</h3>
+<hr>
+<com-grandson></com-grandson>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import comGrandson from './com-grandson.vue'
+
+export default defineComponent({
+  components: { comGrandson },
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
