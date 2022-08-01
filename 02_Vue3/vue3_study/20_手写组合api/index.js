f91a5b4aa57b9dc0cb656f798391e8bbd5634b4c
gd <shengxiaoan@icloud.com>
2022-08-01 22:34:51 +0800

    手写组合api half

---
+++ 02_Vue3/vue3_study/20_手写组合api/index.js
@@ -0,0 +1,10 @@
+// shallowReactive（浅的劫持，浅的监视，浅的响应） 与 reactive（深的）
+
+
+// 定义一个 shallowReactive 函数，传入一个目标对象
+function shallowReactive (target) { 
+  // 判断当前的目标对象是不是object类型（对象/数组）
+
+  // 如果传入的数据是基本类型的数据，那么就直接返回
+  
+}
