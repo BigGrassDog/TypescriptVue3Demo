b4b16d7fc99dfcb2c864a96c0244cc7c289de022
gd <shengxiaoan@icloud.com>
2022-08-09 21:21:28 +0800

    手写 shallowRef 和 ref

--- 02_Vue3/vue3_study/20_手写组合api/index.js
+++ 02_Vue3/vue3_study/20_手写组合api/index.js
@@ -131,3 +131,37 @@ function shallowReadonly (target) {
    // 如果不是对象或者数组，那么直接返回
   return target
 }
+
+// 定义一个 shallowRef 函数
+function shallowRef (target) {
+  return {
+    // 保存target数据保存起来
+    _value: target,
+    get value () { 
+      console.log('劫持到了读取数据');
+      return this._value
+    },
+    set value (val) {
+      console.log('劫持到了修改数据，准备更新界面', val);
+      this._value = val
+     }
+  }
+}
+
+
+// 定义一个 ref 函数
+function ref (target) {
+  target = reactive(target)
+  return {
+    // 保存target数据保存起来
+    _value: target,
+    get value () { 
+      console.log('劫持到了读取数据');
+      return this._value
+    },
+    set value (val) {
+      console.log('劫持到了修改数据，准备更新界面', val);
+      this._value = val
+     }
+  }
+}
