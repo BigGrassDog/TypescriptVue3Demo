60d84644c0896c6ae35fd9a1019984de0a4948ff
gd <shengxiaoan@icloud.com>
2022-08-10 21:52:38 +0800

    综合案例准备

--- 02_Vue3/vue3_study/01_基础代码/22_Teleport组件介绍/App.vue
+++ 02_Vue3/vue3_study/01_基础代码/22_Teleport组件介绍/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-
-  </div>
+<h2>App父级组件</h2>
+<hr>
+<ModalButton></ModalButton>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+import ModalButton from './ModalButton.vue'
 export default defineComponent({
+  name:'App',
+  components:{
+    ModalButton
+  },
   setup () {
     
 
---
+++ 02_Vue3/vue3_study/01_基础代码/23_Suspense组件的使用/App.vue
@@ -0,0 +1,46 @@
+<template>
+<h2>App父级组件：Suspense组件的使用</h2>
+
+<Suspense>
+  <template #default>
+    <!-- 异步组件 -->
+    <!-- <AsyncComponent></AsyncComponent> -->
+    <AsyncAddress></AsyncAddress>
+  </template>
+  <template v-slot:fallback>
+    <!-- loading的内容 -->
+    <h2>loading</h2>
+  </template>
+</Suspense>
+</template>
+
+<script lang="ts">
+import { defineAsyncComponent, defineComponent } from 'vue'
+// 引入组件：静态引入和动态引入
+
+// vue2 中的动态引入组件的写法：(在 vue3 中这种写法不支持)
+// const AsyncComponet = () => import('./AsyncComponent.vue')
+// vue3 中的动态引入组件的写法
+// const AsyncComponent = defineAsyncComponent(
+//   () => import('./AsyncComponent.vue')
+// )
+
+// 静态引入组件
+// import AsyncComponent from './AsyncComponent.vue'
+import AsyncAddress from './AsyncAddress.vue'
+export default defineComponent({
+  name:'App',
+  components:{
+    // AsyncComponent,
+    AsyncAddress
+  },
+  setup () {
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
